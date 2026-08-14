db23e384abc38de52ead75f540f6825fc60546fe
Kacper <kacperrower4@gmail.com>
2026-08-14 16:36:53 +0200

    main

---
+++ Lekcja 1/malib.py
@@ -0,0 +1,12 @@
+print("Czas na gotowanie! Stworzymy najdziwniejsze ciasto świata!")
+
+slodka_rzecz = input("Wpisz coś bardzo słodkiego (np. dżem): ")
+do_picia = input("Wpisz coś do picia (np. woda): ")
+warzywo = input("Wpisz ulubione warzywo: ")
+kolor = input("Wpisz swój ulubiony kolor: ")
+
+print("\n--- PRZEPIS NA CIASTO PROGRAMISTY ---")
+print("1. W dużej misce mieszamy 2 szklanki " + slodka_rzecz + " i 3 kropelki " + do_picia + ".")
+print("2. Dodajemy pokrojone na małe kawałki " + warzywo + " (najlepiej " + kolor + "!)")
+print("3. Całość pieczemy 5 sekund w mikrofalówce i voila!")
+print("Wynik: Ciasto, które pachnie jak " + warzywo + ", ale jest " + slodka_rzecz + " w środku!")
