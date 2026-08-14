d8410c2e1f97651028d966c7132a915de2a8dacb
Kacper <kacperrower4@gmail.com>
2026-08-14 15:36:03 +0200

    elo

---
+++ Lekcja 1/index.py
@@ -0,0 +1,8 @@
+"""Fajny programik XD"""
+
+# print("Siema to jest mój pierwszy program w Pythonie!")
+
+# print(f"To jest mój pierwszy program w Pythonie, a jego nazwa to: {__file__}")
+
+imie = input("Jak masz na imię? ")
+print(f"Siema {imie}")
