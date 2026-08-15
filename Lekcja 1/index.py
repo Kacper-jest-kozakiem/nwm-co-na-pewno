ebcc0efc61ab65b20dc357fad9c5487fbef3a705
Kacper <kacperrower4@gmail.com>
2026-08-15 15:28:38 +0200

    dodano funkcje \n

--- Lekcja 1/index.py
+++ Lekcja 1/index.py
@@ -4,10 +4,10 @@
 
 # print(f"To jest mój pierwszy program w Pythonie, a jego nazwa to: {__file__}")
 
-imie = input("Przetstaw się. ")
+imie = input("Przetstaw się. \n")
 print(f"Siema {imie}")
 
-print(f"Siemanko XD")
+print("Siemanko XD")
 
 print("Siema to jest mój pierwszy program w Pythonie!")
 
